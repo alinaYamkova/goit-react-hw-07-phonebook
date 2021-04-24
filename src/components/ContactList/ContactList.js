e2531b84851cc4f0a423f4a39567fb409e7b0d3b
alinaYamkova <alyamkova@yahoo.com>
2021-04-24 23:22:37 +0200

    save

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Loader from '../../Loader/Loader';
-import PropTypes from "prop-types";
-import s from "../ContactList/phonebook.module.css";
-import contactsOperations from "../../redux/contacts/contacts-operations";
-import contactsSelectors  from "../../redux/contacts/contacts-selectors"
+import PropTypes from 'prop-types';
+import s from '../ContactList/phonebook.module.css';
+import contactsOperations from '../../redux/contacts/contacts-operations';
+import contactsSelectors from '../../redux/contacts/contacts-selectors';
 
 class ContactList extends Component {
   componentDidMount() {
@@ -12,45 +12,51 @@ class ContactList extends Component {
   }
 
   render() {
-    const { contacts, onDeleteContact } = this.props;
-    
+    const { isLoading, contacts, onDeleteContact } = this.props;
+
     return (
       <>
-        {/* {isLoadingContacts && <h1 className={s.loading}><Loader /></h1>} */}
-        <ol>
-          {contacts.map(({ id, name, number }) => (
-            <li className={s.list} key={id}>
-              <p className={s.text}>{name}, </p>
-              <p className={s.text}>tlf.: {number}</p>
-              <button
-                type="button"
-                className={s.btn_del}
-                onClick={() => onDeleteContact(id)}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ol>
+        {isLoading && (
+          <h1 className={s.loading}>
+            <Loader />
+          </h1>
+        )}
+
+        {/* {contacts.length > 0 && ( */}
+          <ol>
+            {contacts.map(({ id, name, number }) => (
+              <li className={s.list} key={id}>
+                <p className={s.text}>{name}, </p>
+                <p className={s.text}>tlf.: {number}</p>
+                <button
+                  type="button"
+                  className={s.btn_del}
+                  onClick={() => onDeleteContact(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ol>
+        {/* )} */}
       </>
     );
-  };
-};
+  }
+}
 
 ContactList.propTypes = {
   contacts: PropTypes.array.isRequired,
   onDeleteContact: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   contacts: contactsSelectors.showFilteredContacts(state),
-  // isLaodingContacts: getLoading(state),
-  filterVal: contactsSelectors.getFilter(state),
+  isLaoding: contactsSelectors.getLoading(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   fetchContact: () => dispatch(contactsOperations.fetchContact()),
-  onDeleteContact: (id) => dispatch(contactsOperations.deleteContact(id)),
+  onDeleteContact: id => dispatch(contactsOperations.deleteContact(id)),
 });
-    
-export default connect(mapStateToProps,mapDispatchToProps) (ContactList);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
